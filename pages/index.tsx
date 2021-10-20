65f09c2eb7aec93c290417ad324477aced12b9e9
Florian LEROUX <florian.leroux3@laposte.net>
2021-10-20 19:19:26 +0200

    feat(charte de l'état): add marianne and colors (awaiting spectral)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>hello rsr</div>;
+  return <div className="p-6 text-4xl font-extrabold uppercase font-marianne text-bleuFrance-500">hello <span className="text-rougeMarianne-500">rsr</span></div>;
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,48 @@
+const colors = require("tailwindcss/colors");
+delete colors.lightBlue;
+
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      marianne: ['"Marianne"', "serif"],
+    },
+    extend: {
+      colors: {
+        ...colors,
+        bleuFrance: {
+          DEFAULT: "#000091",
+          50: "#7878FF",
+          100: "#5E5EFF",
+          200: "#2B2BFF",
+          300: "#0000F7",
+          400: "#0000C4",
+          500: "#000091",
+          600: "#00005E",
+          700: "#00002B",
+          800: "#000000",
+          900: "#000000",
+        },
+        rougeMarianne: {
+          DEFAULT: "#E1000F",
+          50: "#FFC8CB",
+          100: "#FFAEB3",
+          200: "#FF7B84",
+          300: "#FF4854",
+          400: "#FF1525",
+          500: "#E1000F",
+          600: "#AE000C",
+          700: "#7B0008",
+          800: "#480005",
+          900: "#150001",
+        },
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 };
