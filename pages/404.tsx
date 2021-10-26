fadae793a06dba6e13a638fb32a436a68474da9c
Florian LEROUX <florian.leroux3@laposte.net>
2021-10-26 18:54:29 +0200

    feat(layout): store layout done

--- pages/404.tsx
+++ pages/404.tsx
@@ -8,7 +8,7 @@ const Error404: NextPage = () => {
     <AppLayout>
       <main className="flex flex-col items-center justify-center w-full h-full">
         <div className="w-72 h-72">
-          <Lottie
+          {/* <Lottie
             isClickToPauseDisabled
             options={{
               loop: true,
@@ -21,7 +21,7 @@ const Error404: NextPage = () => {
             }}
             // height={"8rem"}
             // width={"8rem"}
-          />
+          /> */}
         </div>
         <h3 className="mb-2 text-3xl font-extrabold text-center text-gray-800 dark:text-gray-200">
           It seems that you are
