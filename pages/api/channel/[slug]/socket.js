15486a6340c34758450fb68b65f9f278da583c80
Ophélie <opheliezeitel@gmail.com>
2021-10-27 09:15:57 +0200

    (add) Add socket.io

---
+++ pages/api/channel/[slug]/socket.js
@@ -0,0 +1,37 @@
+import {Server, Server as ServerIO} from 'socket.io'
+
+const ioHandler = (req, res) => {
+    if (!res.socket.server.io) {
+        console.log('*First use, starting socket.io')
+
+        const io = new Server(res.socket.server)
+
+        io.on('connection', socket => {
+            socket.broadcast.emit('a user connected')
+            socket.on('hello', msg => {
+                socket.emit('hello', 'world!')
+            })
+        })
+
+        res.socket.server.io = io
+    } else {
+        console.log('socket.io already running')
+
+        const io = res.socket.server.io
+
+        io.on('connection', socket => {
+            socket.on('new-message', message => {
+                socket.emit('new-message', 'Received')
+            })
+        })
+    }
+    res.end()
+}
+
+export const config = {
+    api: {
+        bodyParser: false
+    }
+}
+
+export default ioHandler
