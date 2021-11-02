c770a10787442908fc5f720671e826231b307a7a
Florian LEROUX <florian.leroux3@laposte.net>
2021-11-02 21:58:18 +0100

    feat(darkmode): add darkmode toggler + better indicator in sidebar

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -43,25 +43,23 @@ const SideBarIcon = ({
   href: string;
 }) => (
   <Link href={href}>
-    <div className="flex w-full cursor-pointer xl:items-center xl:px-4 group">
+    <div className="flex w-full cursor-pointer xl:items-center xl:px-2 group">
       <div
         className={
           (active
             ? " dark:bg-gray-100  text-blue-500 group-hover:bg-blue-600"
             : "dark:text-gray-100 text-gray-500 group-hover:bg-gray-600 ") +
-          " relative flex-row items-center justify-center w-full min-h-[4rem] min-w-[4rem] mx-auto mt-2 mb-2 transition-all duration-300 ease-linear bg-gray-100 cursor-pointer dark:bg-gray-800 group-hover:text-white group-hover:rounded-lg rounded-xl  flex flex-shrink-0 px-5 "
+          " relative flex-row items-center justify-between w-full min-h-[4rem] min-w-[4rem] mx-auto my-2 transition-all duration-300 ease-linear bg-gray-100 cursor-pointer dark:bg-gray-800 group-hover:text-white group-hover:rounded-lg rounded-xl flex flex-shrink-0 sidebar-icon"
         }
       >
-        
+        {active ? (
+          <span className="w-1 h-6 duration-300 bg-blue-500 rounded-r group-hover:bg-blue-100"></span>
+        ) : (
+          <span></span>
+        )}
         {icon({ className: "flex-shrink-0 w-5 h-5" })}
-        <span
-          className={
-            (active ? "hidden" : "") +
-            " ml-6  absolute w-auto p-2 m-2 text-xs font-bold text-white transition-all duration-100 origin-left scale-0 bg-gray-900 rounded-md shadow-md min-w-max left-14 group-hover:scale-100"
-          }
-        >
-          {text}
-        </span>
+        <span></span>
+
         {/* <span
           className={
             (active ? "flex" : "hidden xl:flex") +
@@ -79,6 +77,14 @@ const SideBarIcon = ({
           {text}
         </span> */}
       </div>
+      <span
+        className={
+          (active ? "hidden" : "") +
+          " ml-6  absolute w-auto p-2 m-2 text-xs font-bold text-white transition-all duration-100 origin-left scale-0 bg-gray-900 rounded-md shadow-md min-w-max left-14 group-hover:scale-100"
+        }
+      >
+        {text}
+      </span>
     </div>
   </Link>
 );
