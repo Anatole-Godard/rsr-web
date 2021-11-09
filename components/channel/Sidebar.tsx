7fc81a4bc250ec08b05699132343a53b2853688e
Florian LEROUX <florian.leroux3@laposte.net>
2021-11-09 21:28:26 +0100

    feat(resource/edit): add edit page

--- components/channel/Sidebar.tsx
+++ components/channel/Sidebar.tsx
@@ -129,7 +129,7 @@ export const Sidebar = ({
         )}
       </div>
       {selectedChannelSlug && (
-        <div className="justify-center hidden w-full p-4  xl:flex">
+        <div className="justify-center hidden w-full p-4 xl:flex">
           {expanded ? (
             <span
               className="flex items-center justify-center p-1 text-blue-500 duration-300 ease-linear bg-blue-200 cursor-pointer active:bg-blue-100 hover:bg-blue-300 rounded-xl dark:bg-blue-700"
