fb720d39e995ab7df567b18d21d569132e5272b3
Anatole-Godard <anatole.godard@icloud.com>
2021-11-11 19:04:15 +0100

    feat(resource admin index): add custom table with fixture

--- pages/admin/resource/index.tsx
+++ pages/admin/resource/index.tsx
@@ -1,12 +1,87 @@
 import { AppLayoutAdmin } from "components/layouts/AppLayoutAdmin";
-import type { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
+import { CustomTable } from '@components/customTable/CustomTable';
 
-const Home: NextPage = () => {
+
+const theadList = [
+  { name : 'type', label : 'Type', width : 20 },
+  { name : 'name', label : 'Nom', width : 80 }
+];
+
+const Dashboard: NextPage<any> = ({
+                                    resources,
+                                  }: {
+  resources: [{
+    type: string;
+    name: string;
+  }];
+}) => {
   return (
     <AppLayoutAdmin>
-      Ressources
+      <CustomTable theadList={theadList} valuesList={resources}/>
     </AppLayoutAdmin>
   );
 };
 
-export default Home;
+export default Dashboard;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return {
+    props : {
+      resources : [
+        {
+          type : "general",
+          name : "General",
+        },
+        {
+          type : "random",
+          name : "Random",
+        },
+        {
+          type : "cool",
+          name : "Cool",
+        },
+        {
+          type : "fun",
+          name : "Fun",
+        },
+        {
+          type : "programming",
+          name : "Programming",
+        },
+        {
+          type : "javascript",
+          name : "Javascript",
+        },
+        {
+          type : "typescript",
+          name : "Typescript",
+        },
+        {
+          type : "react",
+          name : "React",
+        },
+        {
+          type : "node",
+          name : "Node",
+        },
+        {
+          type : "express",
+          name : "Express",
+        },
+        {
+          type : "mongodb",
+          name : "MongoDB",
+        },
+        {
+          type : "mysql",
+          name : "MySQL",
+        },
+        {
+          type : "postgresql",
+          name : "PostgreSQL",
+        },
+      ],
+    },
+  };
+};
