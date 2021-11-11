3a14c2e27e1432800bd23325305638bab711d6fb
Anatole-Godard <anatole.godard@icloud.com>
2021-11-11 18:04:27 +0100

    feat(admin): create tags admin page

---
+++ pages/admin/tag/index.tsx
@@ -0,0 +1,12 @@
+import { AppLayoutAdmin } from "components/layouts/AppLayoutAdmin";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <AppLayoutAdmin>
+      Catégories
+    </AppLayoutAdmin>
+  );
+};
+
+export default Home;
