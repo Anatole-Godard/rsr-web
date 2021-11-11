bcd6335a9b7635a54797556f6314ad5311a4080e
Anatole-Godard <anatole.godard@icloud.com>
2021-11-11 18:03:34 +0100

    feat(admin): create admin home page

---
+++ pages/admin/index.tsx
@@ -0,0 +1,12 @@
+import { AppLayoutAdmin } from "components/layouts/AppLayoutAdmin";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <AppLayoutAdmin>
+      DashBoard
+    </AppLayoutAdmin>
+  );
+};
+
+export default Home;
