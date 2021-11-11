3dfa63fdbc62973c468e14ed46959ec21e36c90b
Florian LEROUX <florian.leroux3@laposte.net>
2021-11-11 19:05:48 +0100

    feat(models): add models (Channel, ChannelInvite) & change var names in olds

---
+++ models/Channel.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const ChannelSchema = new Schema({
+  owner: {
+    type: Object,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  messages: {
+    type: Array,
+  },
+  members: {
+    type: Array,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  resources: {
+    type: Array,
+  },
+  photoURL: {
+    type: String,
+  },
+  visibility: {
+    type: String,
+    enum: ["public", "private"],
+    default: "public",
+  },
+  description: {
+    type: String,
+  },
+});
+
+// mongoose.models = {};
+
+const Channel =
+  mongoose.models.Channel || mongoose.model("Channel", ChannelSchema);
+export default Channel;
