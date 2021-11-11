2972e8042425a6e9f6d48e68c859eacdc2446cf8
Anatole-Godard <anatole.godard@icloud.com>
2021-11-11 18:04:06 +0100

    feat(admin): create resource admin page

---
+++ pages/admin/resource/index.tsx
@@ -0,0 +1,12 @@
+import { AppLayoutAdmin } from "components/layouts/AppLayoutAdmin";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <AppLayoutAdmin>
+      Ressources
+    </AppLayoutAdmin>
+  );
+};
+
+export default Home;
