f793e9075e29364879c6e422b809aca0a723d67f
Florian LEROUX <florian.leroux3@laposte.net>
2021-11-21 17:30:25 +0100

    feat(register): mvp of register page

--- components/navbar/AppNavbar.tsx
+++ components/navbar/AppNavbar.tsx
@@ -1,7 +1,6 @@
 import { UserDropdown } from "@components/dropdowns/UserDropdown";
-import { MoonIcon, SearchIcon, SunIcon } from "@heroicons/react/outline";
-import { useTheme } from "next-themes";
-import Link from "next/link";
+import { Logo } from "@components/ui/Logo";
+import { SearchIcon } from "@heroicons/react/outline";
 
 export const Navbar: React.FC<any> = ({
   config = { shadow: true, backgroundColor: "bg-white dark:bg-gray-900" },
@@ -16,7 +15,7 @@ export const Navbar: React.FC<any> = ({
         " inline-flex items-center justify-between w-full z-40 h-16 px-6 m-0 bg-white dark:bg-black"
       }
     >
-      <Title />
+      <Logo />
       <Search />
       <div className="inline-flex items-center justify-end space-x-4 w-72">
         {/* <ThemeIcon /> */}
@@ -25,7 +24,6 @@ export const Navbar: React.FC<any> = ({
     </div>
   );
 };
-
 
 const Search = () => (
   <div className="items-center hidden w-2/5 px-2 text-gray-500 transition duration-300 ease-in-out bg-gray-100 rounded-md md:flex dark:bg-gray-800 h-9 focus-within:bg-gray-200 font-marianne ring-offset-2 ring-blue-500 focus-within:ring-2">
@@ -36,13 +34,4 @@ const Search = () => (
     />
     <SearchIcon className="w-5 h-5 my-auto text-secondary" />
   </div>
-);
-
-const Title = () => (
-  <Link href="/">
-    <a className="inline-flex items-center my-auto ml-2 text-xl font-extrabold tracking-wider text-gray-500 uppercase transition duration-300 ease-in-out w-72 text-opacity-80">
-      <span className="text-bleuFrance-500">Hello</span>{" "}
-      <span className="ml-1 text-rougeMarianne-500">RSR</span>
-    </a>
-  </Link>
 );
