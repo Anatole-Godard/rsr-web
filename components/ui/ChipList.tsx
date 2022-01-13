7a7fa5654df7ffc6bce1e5bd8adb922d340f448c
Anatole-Godard <anatole.godard@icloud.com>
2022-01-13 16:31:29 +0100

    feat(users admin roles): update roles

--- components/ui/ChipList.tsx
+++ components/ui/ChipList.tsx
@@ -8,12 +8,14 @@ export const ChipList = ({
   setSelected = null,
   color = "green",
   size = "normal",
+  isOnlyOne = false,
 }: {
-  list: string[];
-  selected?: string[];
-  setSelected?: ((selected: string[]) => void) | Dispatch<SetStateAction<string[]>> | null;
+  list: {label, value}[];
+  selected?: {label, value}[];
+  setSelected?: ((selected?: {label, value}[]) => void) | Dispatch<SetStateAction<{label, value}[]>> | null;
   color?: string;
   size: "normal" | "small";
+  isOnlyOne?: boolean;
 }) => {
   return (
     <div
@@ -22,23 +24,26 @@ export const ChipList = ({
         size === "normal" ? "space-x-3 p-1.5" : " space-x-1 p-0.5"
       )}
     >
-      {list.map((el, index) => (
-        <Chip
-          color={color}
-          name={el}
-          key={index}
-          size={size}
-          checked={selected.findIndex((chip) => chip === el) !== -1}
-          onClick={
-            setSelected
-              ? () =>
-                  selected.findIndex((chip) => chip === el) === -1
-                    ? setSelected([...selected, el])
-                    : setSelected(selected.filter((chip) => chip !== el))
-              : null
-          }
-        />
-      ))}
+      {list.map((el, index) => {
+        const accSelected = isOnlyOne ? [] : [...selected]
+        return(
+            <Chip
+                color={color}
+                element={el}
+                key={index}
+                size={size}
+                checked={selected.findIndex((chip) => chip.label === el.label) !== -1}
+                onClick={
+                  setSelected
+                      ? () =>
+                          selected.findIndex((chip) => chip.label === el.label) === -1
+                              ? setSelected([...accSelected, el])
+                              : setSelected(selected.filter((chip) => chip.label !== el.label))
+                      : null
+                }
+            />
+        )
+      })}
     </div>
   );
 };
