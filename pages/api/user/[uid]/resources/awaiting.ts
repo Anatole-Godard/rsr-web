53c7e455a0608d56a9dd7705c726f93aff0cc621
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-02-24 14:27:33 +0100

    feat(api): add awaiting validation route

---
+++ pages/api/user/[uid]/resources/awaiting.ts
@@ -0,0 +1,52 @@
+import { Resource } from "@definitions/Resource";
+import { withAuth } from "@middleware/auth";
+import withDatabase from "@middleware/mongoose";
+import ResourceModel from "@models/Resource";
+import { handleError } from "@utils/handleError";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { uid: queryUid } = req.query as { uid: string };
+    const { uid: headersUid } = req.headers as { uid: string };
+
+    if (!queryUid || !headersUid)
+      return res.status(400).json({
+        data: null,
+        error: {
+          message: "Missing uid",
+          code: 400,
+          name: "MissingUidError",
+        },
+      });
+
+    if (queryUid !== headersUid)
+      return res.status(401).json({
+        data: null,
+        error: {
+          code: 401,
+          message: "Unauthorized",
+          name: "UnauthorizedError",
+          user: headersUid,
+        },
+      });
+
+    const resources: Resource[] = await ResourceModel.find({
+      "owner.uid": queryUid,
+      validated: false,
+    });
+
+    return res.status(200).json({
+      data: {
+        attributes: resources,
+        type: "resource",
+        id: "awaitingValidation",
+      },
+      error: null,
+    });
+  } catch (err) {
+    handleError(res, err, req.url);
+  }
+};
+
+export default withAuth(withDatabase(handler));
