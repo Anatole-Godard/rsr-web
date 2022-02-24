7916d1c7f3e2c7bbcefac20b5cb8af9f012a4d7e
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-02-24 19:16:51 +0100

    ref(card): change card to match new needs

--- constants/resourcesTypes.ts
+++ constants/resourcesTypes.ts
@@ -2,11 +2,17 @@ import {
   LinkIcon as ExternalLinkIconOutline,
   HandIcon as HandIconOutline,
   LocationMarkerIcon as LocationMarkerIconOutline,
+  LockClosedIcon as LockClosedIconOutline,
+  GlobeIcon as GlobeIconOutline,
+  BanIcon as BanIconOutline,
 } from "@heroicons/react/outline";
 import {
   LinkIcon as ExternalLinkIconSolid,
   HandIcon as HandIconSolid,
   LocationMarkerIcon as LocationMarkerIconSolid,
+  LockClosedIcon as LockClosedIconSolid,
+  GlobeIcon as GlobeIconSolid,
+  BanIcon as BanIconSolid,
 } from "@heroicons/react/solid";
 
 export const types = [
@@ -37,24 +43,34 @@ export type ResourceType = {
   label: string;
   value: string;
   hasImage: boolean;
+  icon: Icon;
 };
 
 export const visibilities = [
   {
     label: "Publique",
     value: "public",
+    icon: { outline: GlobeIconOutline, solid: GlobeIconSolid },
   },
   {
     label: "Privée",
     value: "private",
+    icon: { outline: LockClosedIconOutline, solid: LockClosedIconSolid },
   },
   {
     label: "Non répertoriée",
     value: "unlisted",
+    icon: { outline: BanIconOutline, solid: BanIconSolid },
   },
 ];
 
 export type ResourceVisibility = {
   label: string;
   value: string;
+  icon: Icon;
+};
+
+type Icon = {
+  outline: (props: React.ComponentProps<"svg">) => JSX.Element;
+  solid: (props: React.ComponentProps<"svg">) => JSX.Element;
 };
