10a8ecf16dfc1473a16ba4edfe0696d27a2dea4b
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-03-08 17:06:51 +0100

    ref(logo): column instead of row

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -5,11 +5,11 @@ export const Logo = ({ className }: { className?: string }) => (
     <a
       className={
         className ||
-        "flex flex-col xl:flex-row xl:items-center xl:ml-2 lg:pr-0 shrink-0 text-sm xl:text-xl font-extrabold tracking-wider text-gray-500 uppercase transition duration-300 ease-in-out text-opacity-80"
+        "flex flex-col mx-auto w-max xl:mt-2 xl:my-6 lg:pr-0 shrink-0 text-sm xl:text-xl font-extrabold tracking-wider text-gray-500 uppercase transition duration-300 ease-in-out text-opacity-80"
       }
     >
       <span className="flex text-bleuFrance-500">Hello</span>{" "}
-      <span className="-mt-2 xl:mt-0 xl:ml-1 text-rougeMarianne-500">RSR</span>
+      <span className="-mt-2 text-rougeMarianne-500">RSR</span>
     </a>
   </Link>
 );
