c0d6e5b4653b234d9fad8b3890ac23bf459d0ec6
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-06-15 15:12:18 +0200

    ref: resource view

--- components/ui/Chip.tsx
+++ components/ui/Chip.tsx
@@ -14,9 +14,9 @@ export const Chip = ({
   size: "normal" | "small";
 }) => {
   let chipClassName =
-    "inline-flex items-center justify-center  font-medium transition duration-300 rounded-full select-none " +
+    "inline-flex items-center justify-center  font-medium transition duration-300 rounded-md select-none " +
     (onClick ? "cursor-pointer " : " ") +
-    (size === "normal" ? "px-4 py-2" : "px-1.5 py-1");
+    (size === "normal" ? "px-3 py-1.5" : "px-1.5 py-1");
   switch (color) {
     case "green":
       chipClassName +=
