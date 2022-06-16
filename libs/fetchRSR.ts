0cbf441f125328f3f48775ba941d912a9fbacfbc
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-06-16 13:57:00 +0200

    fix: minor fixes

--- libs/fetchRSR.ts
+++ libs/fetchRSR.ts
@@ -1,9 +1,11 @@
+import toast from "react-hot-toast";
+
 /**
  * It fetches a resource from the RSR API.
- * 
+ *
  * TODO: add revalidate jwt method if (response === 401 || response === 403)
  * TODO: mutate user object when refreshing token
- * 
+ *
  * @param {string} url - The URL to fetch.
  * @param {any} session - the session object that contains the token and uid
  * @param {any} [options] - any
@@ -11,26 +13,45 @@
  */
 export const fetchRSR = async (
   url: string,
-  session: any, // TODO: check if we can only define one part of the type here
+  session: { token: string; uid: string },
   options?: any
 ): Promise<Response> => {
-  try {
-    return fetch(url, {
-      ...options,
-      headers: {
-        Authorization: "Bearer " + session.token,
-        ...options?.headers,
-        appsource: "web",
-        "Content-Type": "application/json",
-        uid: session.uid,
-      },
+  if (!url) throw new Error("url is required");
+  if (!session) throw new Error("session is required");
+  if (!session.token) throw new Error("session.token is required");
+  if (!session.uid) throw new Error("session.uid is required");
+
+  return fetch(url, {
+    ...options,
+    headers: {
+      Authorization: "Bearer " + session.token,
+      ...options?.headers,
+      appsource: "web",
+      "Content-Type": "application/json",
+      uid: session.uid,
+    },
+  })
+    .then((res) => {
+      if (res.status === 401 || res.status === 403) {
+        toast.error("Votre session a expiré, veuillez vous reconnecter");
+      }
+
+      if (res.ok) {
+        return Promise.resolve(res);
+      } else {
+        toast.error(
+          (res as unknown as { error: { client: string } })?.error?.client ||
+            "Une erreur est survenue"
+        );
+      }
+    })
+    .catch((err) => {
+      toast.error(err.message);
+      return Promise.reject({
+        error: {
+          message: err.message,
+          location: "fetchRSR",
+        },
+      });
     });
-  } catch (err) {
-    return Promise.reject({
-      error: {
-        message: err.message,
-        location: "fetchRSR",
-      },
-    });
-  }
 };
