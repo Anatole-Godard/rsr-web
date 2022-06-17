573237c0d55e3b8359e8935dbda6780395555cd4
Florian Leroux <10078837+floriaaan@users.noreply.github.com>
2022-06-17 11:33:03 +0200

    Update utils/toMinimum.ts

--- utils/toMinimum.ts
+++ utils/toMinimum.ts
@@ -4,7 +4,7 @@ import { Resource, ResourceMinimum } from "@definitions/Resource";
  * `toResourceMinimum` takes a `Resource` and returns a `ResourceMinimum`
  * @param {Resource} resource - The resource to be converted.
  */
-export const toResourceMinimum = (resource: Resource): { owner: UserMinimum; createdAt: Date | string; data: { type: "location" | "physical_item" | "external_link" | "event" | string; attributes: any }; validated: boolean; visibility: "public" | "private" | "unlisted"; members: UserMinimum[]; description: string; slug: string; tags: TagDocument[] | string[]; seenBy: UserMinimum[] } => ({
+export const toResourceMinimum = (resource: Resource): ResourceMinimum => ({
   slug: resource.slug,
   description: resource.description,
   tags: resource.tags,
