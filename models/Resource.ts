5a2c38e941528cff1aee5f1f86ed48326c2de870
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-06-22 22:29:56 +0200

    fix(build): workaround in ResourceSchema

--- models/Resource.ts
+++ models/Resource.ts
@@ -68,6 +68,6 @@ export default Resource;
 // @ts-ignore
 ResourceSchema.post("save", function (next) {
   // workaround for passing builds
-  (this as { updatedAt: number }).updatedAt = Date.now();
+  (this as unknown as { updatedAt: number }).updatedAt = Date.now();
   next();
 });
