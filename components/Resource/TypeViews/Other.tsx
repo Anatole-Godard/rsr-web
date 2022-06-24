4f517c79e0cde1a9a84a9463942743e2d76822c4
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-06-24 23:32:33 +0200

    feat(i18n): add i18n for resource/all and resource/slug

--- components/Resource/TypeViews/Other.tsx
+++ components/Resource/TypeViews/Other.tsx
@@ -8,6 +8,7 @@ import {
   QuestionMarkCircleIcon,
 } from "@heroicons/react/outline";
 import { toModularInput } from "@utils/toModularInput";
+import { useTranslations } from "next-intl";
 import { Fragment } from "react";
 
 interface OtherViewProps {
@@ -16,9 +17,8 @@ interface OtherViewProps {
   updatedAt: string;
 }
 
-export const OtherView = ({ attributes, slug, updatedAt }: OtherViewProps) => {
-  console.log(updatedAt);
-
+export const OtherView = ({ attributes, updatedAt }: OtherViewProps) => {
+  const t = useTranslations("ResourceView");
   return (
     <>
       <div className="relative min-h-[18rem] md:min-h-0 h-full overflow-hidden rounded-lg xl:col-span-2">
@@ -30,10 +30,10 @@ export const OtherView = ({ attributes, slug, updatedAt }: OtherViewProps) => {
         ) : (
           <div className="flex flex-col items-center justify-center h-full text-gray-800 bg-gray-200 dark:text-gray-200 dark:bg-gray-800">
             <QuestionMarkCircleIcon className="w-12 h-12 mb-1" />
-            <p className="text-lg font-spectral">Autre</p>
+            <p className="text-lg font-spectral">{t("other")}</p>
             {!attributes.properties.image && (
               <p className="pt-2 mx-12 mt-2 text-sm text-center text-gray-600 border-t border-gray-500 dark:text-gray-400 font-spectral">
-                {"Aucun média n'est disponible pour cette ressource"}
+                {t("no-media")}
               </p>
             )}
           </div>
@@ -48,25 +48,25 @@ export const OtherView = ({ attributes, slug, updatedAt }: OtherViewProps) => {
                 {input.type === "string" && (
                   <span className="btn-bleuFrance text-xs px-1 py-0.5">
                     <MenuAlt2Icon className="w-3 h-3 mr-1" />
-                    Texte
+                    {t(input.type)}
                   </span>
                 )}
                 {input.type === "date" && (
                   <span className="btn-bleuFrance text-xs px-1 py-0.5">
                     <CalendarIcon className="w-3 h-3 mr-1" />
-                    Date
+                    {t(input.type)}
                   </span>
                 )}
                 {input.type === "number" && (
                   <span className="btn-bleuFrance text-xs px-1 py-0.5">
                     <CalculatorIcon className="w-3 h-3 mr-1" />
-                    Nombre
+                    {t(input.type)}
                   </span>
                 )}
                 {input.type === "boolean" && (
                   <span className="btn-bleuFrance text-xs px-1 py-0.5">
                     <CheckCircleIcon className="w-3 h-3 mr-1" />
-                    Oui/Non
+                    {t(input.type)}
                   </span>
                 )}
               </h4>
@@ -74,8 +74,8 @@ export const OtherView = ({ attributes, slug, updatedAt }: OtherViewProps) => {
                 {input.type !== "boolean"
                   ? input.value
                   : input.value
-                  ? "Oui"
-                  : "Non"}
+                  ? t("boolean-true")
+                  : t("boolean-false")}
               </p>
             </div>
             <hr className="mx-5 mb-3 border-gray-300 dark:border-gray-700 last:border-0" />
