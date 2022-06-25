7ea99b47f384bd24bce49df3e243b6299e49d54f
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-06-26 00:25:58 +0200

    feat(#31): add title for admin, auth pages

--- pages/admin/report/index.tsx
+++ pages/admin/report/index.tsx
@@ -107,7 +107,7 @@ const ReportAdmin: NextPage<any> = (props) => {
 
   return (
     <>
-      <AdminLayout>
+      <AdminLayout title="Signalements - Admin.">
         <div className="flex flex-col w-full h-full bg-white dark:bg-black grow">
           <div className="flex flex-col w-full px-6 py-6 bg-white shrink-0 lg:px-12 dark:bg-black dark:border-gray-800">
             <div className="inline-flex items-end justify-between w-full mb-2">
--- pages/admin/tags/index.tsx
+++ pages/admin/tags/index.tsx
@@ -113,7 +113,7 @@ const TagManager: NextPage<any> = (props) => {
   const [search, setSearch] = useState<string>("");
 
   return (
-    <AdminLayout>
+    <AdminLayout title="Tags - Admin.">
       <div className="flex flex-col w-full h-full bg-white dark:bg-black grow">
         <div className="flex flex-col w-full px-6 py-6 bg-white shrink-0 lg:px-12 dark:bg-black dark:border-gray-800">
           <div className="inline-flex items-end justify-between w-full mb-2">
