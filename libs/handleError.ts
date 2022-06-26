66572059841f94404e12657e12201c8e096bb72d
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-06-26 17:25:26 +0200

    test: add pagination tests

--- libs/handleError.ts
+++ libs/handleError.ts
@@ -5,13 +5,13 @@ import { NextApiResponse } from "next";
  * the error is sent to the client with a 500 status code
  * @param {NextApiResponse} res - NextApiResponse
  * @param {Error} error - The error object that was thrown.
- * @param {Function | string} fnc - Function | string
+ * @param {Function | string} location - Function | string
  */
 export const handleError = (
   res: NextApiResponse,
   error: Error,
   location: Function | string,
-  client: string = "Une erreur est survenue"
+  // client: string = "Une erreur est survenue"
 ) => {
   const { message = "internal server error", stack } = error;
 
