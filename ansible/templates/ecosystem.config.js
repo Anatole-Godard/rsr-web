afd128ef6d57443c063de1d09baffbd59cbe444a
ophzl <opheliezeitel@gmail.com>
2022-07-04 00:52:27 +0200

    (wip) Actions

--- ansible/templates/ecosystem.config.js
+++ ansible/templates/ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   apps: [{
-    name: 'next', // App name that shows in `pm2 ls`
+    name: 'rsr', // App name that shows in `pm2 ls`
     exec_mode: 'cluster', // enables clustering
-    instances: 2, // or an integer
+    instances: 1, // or an integer
     script: "./node_modules/next/dist/bin/next",
     args: "start",
-    cwd: "/home/admin/current/"
+    cwd: "/home/ophzl/app/"
   }],
 };
