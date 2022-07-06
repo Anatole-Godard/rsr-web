bab048e14c57544790d735b411be116eea505ac0
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-07-06 22:15:15 +0200

    feat(fetchers): add logging to fetcher

---
+++ models/Log.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const logSchema = new Schema({
+  user: {
+    type: Object,
+    required: true,
+  },
+  method: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: ["fetch", "next"],
+  },
+});
+
+// mongoose.models = {};
+
+const Log = mongoose.models.Log || mongoose.model("Log", logSchema);
+export default Log;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,26 @@
-import 'leaflet/dist/leaflet.css';
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { ThemeProvider } from 'next-themes';
-import { DarkModeToggler } from '@components/Helper/DarkModeToggler';
-import { AnimatePresence } from 'framer-motion';
-import { AuthProvider } from '@hooks/useAuth';
-import { NotificationProvider } from '@hooks/useNotifications';
-import { GetStarted } from '@components/Helper/GetStarted';
-import { Toaster } from 'react-hot-toast';
-import { ReportProvider } from '@hooks/useReport';
-import { NextIntlProvider } from 'next-intl';
+import "leaflet/dist/leaflet.css";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
+import { DarkModeToggler } from "@components/Helper/DarkModeToggler";
+import { AnimatePresence } from "framer-motion";
+import { AuthProvider } from "@hooks/useAuth";
+import { NotificationProvider } from "@hooks/useNotifications";
+import { GetStarted } from "@components/Helper/GetStarted";
+import { Toaster } from "react-hot-toast";
+import { ReportProvider } from "@hooks/useReport";
+import { NextIntlProvider } from "next-intl";
+import { Logger } from "@components/Helper/Logger";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute='class'>
+    <ThemeProvider attribute="class">
       <NextIntlProvider messages={pageProps.i18n}>
         <AuthProvider>
           <NotificationProvider>
             <ReportProvider>
               <AnimatePresence onExitComplete={() => window.scrollTo(0, 0)}>
-                <div className='relative' key={'app'}>
+                <div className="relative" key={"app"}>
                   <Component {...pageProps} />
                   <DarkModeToggler />
                   <GetStarted />
@@ -28,7 +29,8 @@ function MyApp({ Component, pageProps }: AppProps) {
             </ReportProvider>
           </NotificationProvider>
         </AuthProvider>
-        <Toaster position='bottom-right' />
+        <Logger />
+        <Toaster position="bottom-right" />
       </NextIntlProvider>
     </ThemeProvider>
   );
