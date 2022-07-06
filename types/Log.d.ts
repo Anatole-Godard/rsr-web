cb7590c4bbad9372465e876619869d0ca2b83c2c
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-07-06 22:15:15 +0200

    feat(fetchers): add logging to fetcher

---
+++ types/Log.d.ts
@@ -0,0 +1,17 @@
+import { UserMinimum } from "./User";
+
+export type LogBody = {
+  method: string;
+  location: string;
+  user: UserMinimum;
+  url: string;
+  type: "fetch" | "next";
+};
+
+export type LogOptions = {
+  method: "POST";
+  headers: {
+    "Content-Type": "application/json";
+  };
+  body: string;
+};
